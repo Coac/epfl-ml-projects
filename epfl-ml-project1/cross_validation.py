913651dd30a68d4c09c249bc04839238a59f7218
Coac <levictor3000@hotmail.fr>
2017-10-15 17:16:50 +0200

    Moved to cross valid

--- epfl-ml-project1/cross_validation.py
+++ epfl-ml-project1/cross_validation.py
@@ -1,4 +1,4 @@
-import numpy as np
+from gradient_descent import *
 
 
 def separate_set(x, y):
@@ -18,3 +18,46 @@ def separate_set(x, y):
     test_x = test_set[:, 1:]
 
     return train_x, train_y, test_x, test_y
+
+
+def build_k_indices(y, k_fold, seed=1):
+    """build k indices for k-fold."""
+    num_row = y.shape[0]
+    interval = int(num_row / k_fold)
+    np.random.seed(seed)
+    indices = np.random.permutation(num_row)
+    k_indices = [indices[k * interval: (k + 1) * interval]
+                 for k in range(k_fold)]
+    return np.array(k_indices)
+
+
+def k_fold_cross_validation(y, x, k, lambda_, predict_threshold=0):
+    """return the accuracy of ridge regression."""
+
+    k_indices = build_k_indices(y, k)
+
+    accuracy_tr = []
+    accuracy_te = []
+
+    for i in range(0, k):
+        i = 0
+
+        # get k'th subgroup in test, others in train:
+        x_test = x[k_indices[i]]
+        y_test = y[k_indices[i]]
+        x_train = np.array([]).reshape(0, x.shape[1])
+        y_train = np.array([]).reshape(0, 1)
+
+        for j in range(0, k):
+            if j != i:
+                x_train = np.concatenate((x_train, x[k_indices[j]]))
+                y_train = np.concatenate((y_train, y[k_indices[j]]))
+
+        # ridge regression:
+        w, loss = ridge_regression(y_train, x_train, lambda_)
+
+        # calculate the loss for train and test data
+        accuracy_tr.append(get_accuracy(x_train, y_train, w, predict_threshold))
+        accuracy_te.append(get_accuracy(x_test, y_test, w, predict_threshold))
+
+    return np.mean(accuracy_tr), np.mean(accuracy_te)
